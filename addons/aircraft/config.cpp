6801edd372c07428f5509be7b2e16f6e1345f011
Weber <mcnameeryanj@gmail.com>
2025-07-17 21:28:32 -0500

    Falcons Complete Baring Final Textures

--- addons/aircraft/config.cpp
+++ addons/aircraft/config.cpp
@@ -22,6 +22,13 @@ class CfgVehicles
     class Turrets;
     class LeftDoorGun;
     class RightDoorGun;
+    class CargoTurret_01;
+    class CargoTurret_02;
+    class CargoTurret_03;
+    class CargoTurret_04;
+    class CargoTurret_05;
+    class CargoTurret_06;
+    class CargoTurret_07;
   };
   class SFT_MH144_S_Falcon : OPTRE_UNSC_MH_144S_Falcon
   {
@@ -74,16 +81,54 @@ class CfgVehicles
         weapons[] = {"SFT_247H_Sidegun2"};
         magazines[] = {"OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE"};
       };
+      class CargoTurret_01 : CargoTurret_01{};
+      class CargoTurret_02 : CargoTurret_02{};
+      class CargoTurret_03 : CargoTurret_03{};
+      class CargoTurret_04 : CargoTurret_04{};
+      class CargoTurret_05 : CargoTurret_05{};
+      class CargoTurret_06 : CargoTurret_06{};
+      class CargoTurret_07 : CargoTurret_07{};
     };
   };
-  class OPTRE_UNSC_falcon_armed;
+  class OPTRE_UNSC_MH_144_Falcon;
+  class OPTRE_UNSC_falcon_armed : OPTRE_UNSC_MH_144_Falcon
+  {
+    class Turrets;
+    class LeftDoorGun;
+    class RightDoorGun;
+    class CargoTurret_01;
+    class CargoTurret_02;
+    class CargoTurret_03;
+    class CargoTurret_04;
+    class CargoTurret_05;
+    class CargoTurret_06;
+    class CargoTurret_07;
+    class CargoTurret_08;
+    class CargoTurret_09;
+  };
   class SFT_AH144_Falcon : OPTRE_UNSC_falcon_armed
   {
     author = "Weber";
     faction = "SFT_BLUE_SFT_F";
     displayName = "[SFT] AH-144 'Falcon' [DAP]";
-    weapons[] = {"autocannon_40mm_CTWS","CMFlareLauncher","Laserdesignator_pilotCamera"};
-    magazines[] = {"SFT_2000Rnd_40mm_HE","SFT_2000Rnd_40mm_HE","SFT_2000Rnd_40mm_HE","SFT_2000Rnd_40mm_HE"};
+    weapons[] = 
+    {
+      "autocannon_40mm_CTWS",
+      "CMFlareLauncher",
+      "Laserdesignator_pilotCamera"
+    };
+    magazines[] = 
+    {
+      "60Rnd_40mm_GPR_Tracer_Red_shells",
+      "60Rnd_40mm_GPR_Tracer_Red_shells",
+      "60Rnd_40mm_GPR_Tracer_Red_shells",
+      "40Rnd_40mm_APFSDS_Tracer_Red_shells",
+      "40Rnd_40mm_APFSDS_Tracer_Red_shells",
+      "40Rnd_40mm_APFSDS_Tracer_Red_shells",
+      "168Rnd_CMFlare_Chaff_Magazine",
+      "168Rnd_CMFlare_Chaff_Magazine",
+      "Laserbatteries"
+    };
     hiddenSelectionsTextures[] =
     {
       FALCON_HIDDENSELECTIONSTEXTURES_STANDARD
@@ -100,17 +145,17 @@ class CfgVehicles
         };
       };
     };
-    class Turrets
+    class Turrets : Turrets
     {
-      class LeftDoorGun
-      {
-        weapons[] = {"SFT_247H_Sidegun"};
-				magazines[] = {"OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE","OPTRE_2000Rnd_20mm_HE"};
-      };
-      class RightDoorGun
-      {
-        weapons[] = {"SFT_247H_Sidegun2"};
-      };
+      class CargoTurret_01 : CargoTurret_01{};
+      class CargoTurret_02 : CargoTurret_02{};
+      class CargoTurret_03 : CargoTurret_03{};
+      class CargoTurret_04 : CargoTurret_04{};
+      class CargoTurret_05 : CargoTurret_05{};
+      class CargoTurret_06 : CargoTurret_06{};
+      class CargoTurret_07 : CargoTurret_07{};
+      class CargoTurret_08 : CargoTurret_08{};
+      class CargoTurret_09 : CargoTurret_09{};
     };
   };
   class OPTRE_UNSC_falcon_medical;
