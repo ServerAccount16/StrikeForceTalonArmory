35cd0ef783b044d68433f2d6c679260ba4b25713
Weber <mcnameeryanj@gmail.com>
2025-07-19 21:51:14 -0500

    TEST

---
+++ addons/map_markers/config.cpp
@@ -0,0 +1,353 @@
+class CfgPatches
+{
+    class map_markers
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion=1.00;
+    };
+};
+class CfgMarkerClasses
+{
+    class Halo_map_markers_Facs_SA
+	{
+		displayName="Halo Factions";
+    };
+
+    class Halo_map_markers_Ships_SA
+	{
+		displayName="Halo Ships";
+    };
+};
+class CfgMarkers
+{
+    class UNSC_SA
+    {
+        name="[Halo] UNSC";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\UNSC_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class UNSC_Army_SA
+    {
+        name="[Halo] UNSC Army";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\UNSC_Army_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class UNSC_Marine_SA
+    {
+        name="[Halo] UNSC Marine Corps";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\UNSC_Marines_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class UNSC_Navy_SA
+    {
+        name="[Halo] UNSC Navy";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\UNSC_Navy_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class ODST_SA
+    {
+        name="[Halo] ODST";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\ODST_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class ONI_SA
+    {
+        name="[Halo] ONI";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\ONI_SA.paa";
+        color[]={0,0,0,0};
+        size=42;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class CMA_SA
+    {
+        name="[Halo] CMA";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\CMA_SA.paa";
+        color[]={0,0,0,0};
+        size=42;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Hastati_SA
+    {
+        name="[Halo] Hastati Company";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Hastati_SA.paa";
+        color[]={1,1,1,1};
+        size=42;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class WhiteTeam_SA
+    {
+        name="[Halo] SII White Team";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\WhiteTeam_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Flood_SA
+    {
+        name="[Halo] Flood";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Flood_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Forerunner_SA
+    {
+        name="[Halo] Forerunner";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Forerunner_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Banished_SA
+    {
+        name="[Halo] Banished";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Banished_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Covenant_CE_SA
+    {
+        name="[Halo] Covenant";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Covenant_CE_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Covenant_HW_SA
+    {
+        name="[Halo] Covenant (HW)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Covenant_HW_SA.paa";
+        color[]={0,0,0,0};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Insurrectionist_SA
+    {
+        name="[Halo] Insurrectionist";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Insurrectionist_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Insurrectionist2_SA
+    {
+        name="[Halo] Insurrectionist (Version 2)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Insurrectionist_2_SA.paa";
+        color[]={0,0,0,0};
+        size=42;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class Insurrectionist3_SA
+    {
+        name="[Halo] Insurrectionist (Version 3)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Insurrectionist_3_SA.paa";
+        color[]={0,0,0,0};
+        size=42;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class SoS_SA
+    {
+        name="[Halo] Swords of Sanghelios";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\SoS_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Facs_SA";
+    };
+
+    class CAS_SA
+    {
+        name="[Halo] Assault Carrier (CAS)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\CAS_SA.paa";
+        color[]={0,0,0,1};
+        size=90;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class CCS_SA
+    {
+        name="[Halo] Battlecruiser (CCS)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\CCS_SA.paa";
+        color[]={0,0,0,1};
+        size=80;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class CPV_SA
+    {
+        name="[Halo] Battlecruiser (CPV)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\CPV_SA.paa";
+        color[]={0,0,0,1};
+        size=80;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class SDV_SA
+    {
+        name="[Halo] Heavy Corvette (SDV)";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\SDV_SA.paa";
+        color[]={0,0,0,1};
+        size=60;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class D20_SA
+    {
+        name="[Halo] D20 Heron";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\D20Her_SA.paa";
+        color[]={0,0,0,1};
+        size=60;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Frigate_SA
+    {
+        name="[Halo] Frigate";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Frigate_SA.paa";
+        color[]={0,0,0,1};
+        size=70;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Drake_SA
+    {
+        name="[Halo] Drake-class";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Drakeclass_SA.paa";
+        color[]={0,0,0,1};
+        size=55;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Halberd_SA
+    {
+        name="[Halo] Halberd-class";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Halberd_SA.paa";
+        color[]={0,0,0,1};
+        size=80;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Phoenix_SA
+    {
+        name="[Halo] Phoenix-class";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Phoenix_SA.paa";
+        color[]={0,0,0,1};
+        size=70;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Pillar_SA
+    {
+        name="[Halo] Halcyon-class";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Pillar_SA.paa";
+        color[]={0,0,0,1};
+        size=85;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Longsword_SA
+    {
+        name="[Halo] Longsword";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Longsword_SA.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+
+    class Pelican_SA
+    {
+        name="[Halo] Pelican";
+        icon="\x\strikeforcetalon_armory\addons\sft_map_markers\markers\Pelican_SA.paa";
+        color[]={0,0,0,1};
+        size=32;
+        shadow = 0;
+        scope = 2;
+        markerClass = "Halo_map_markers_Ships_SA";
+    };
+};
